a929878cfc5737853dab39f419e1f6a8b4bc8ab3
Adam Keating <adam.keating@fsae.co.nz>
2023-02-01 12:04:37 +1300

    Initialised project files and created basic publisher node

---
+++ EBS/EBS_node.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Bool
+from sensor_msgs.msg import LaserScan
+
+class ebsNode(Node):
+    def __init__(self):
+        super().__init__("ebsNode")
+        self.ebs_pub_ = self.create_publisher(Bool, "/trigger_ebs", 10)
+        self.ebs_sub_ = self.create_subscription(LaserScan, "/lidar", self.on_msg, 10)
+        self.get_logger().info("ebsNode Initialised")
+
+    def on_msg(self, msg):
+        self.ebs_pub_.publish("received msg")
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    node = ebsNode()
+    rclpy.spin(node)
+
+    rclpy.shutdown()
